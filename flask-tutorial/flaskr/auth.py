ab435f46e809a022f2fd5e0dde4cc16dde6e5761
Steven Tran <dpoopd@gmail.com>
2023-05-28 17:10:30 -0700

    Completed templates

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -29,6 +29,7 @@ def register():
             try:
                 db.execute(
                     "INSERT INTO user (username, password) VALUES (?, ?)",
+                    # generate_password_hash() is used to securely hash the pw
                     (username, generate_password_hash(password)),
                 )
                 db.commit()
@@ -40,3 +41,61 @@ def register():
         flash(error)
 
     return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        
+        flash(error)
+    
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        # g.user will be available in other views
+        g.user = get_db().execute(
+            "SELECT * FROM user WHERE id = ?", (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    """
+    Remove the user id from the session.
+    """
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        """
+        Check if a user is loaded and redirect to the login page otherwise.
+        """
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        
+        return view(**kwargs)
+    return wrapped_view
