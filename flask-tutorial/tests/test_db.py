b47ac9ff33b53aa408b8f929cbc15f48e61a1655
Steven Tran <dpoopd@gmail.com>
2023-05-29 14:43:15 -0700

    Added sql db tests

---
+++ flask-tutorial/tests/test_db.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+import pytest
+from flaskr.db import get_db
+
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT 1')
+
+    # ProgrammingError is raised if the connection was closed.
+    assert 'closed' in str(e.value)
+
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+
+    def fake_init_db():
+        Recorder.called = True
+
+    # Monkeypatching is used to replace init_db with one that records that it’s been called.
+    monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
