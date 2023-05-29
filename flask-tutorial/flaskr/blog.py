e59a75503638857e6200b6f2fc0849310cf282ed
Steven Tran <dpoopd@gmail.com>
2023-05-28 17:52:08 -0700

    Added update() function

--- flask-tutorial/flaskr/blog.py
+++ flask-tutorial/flaskr/blog.py
@@ -60,3 +60,33 @@ def get_post(id, check_author=True):
         abort(403)
 
     return post
+
+# Flask ensures id is is an int, and pass it as the id argument
+bp.route('/<int:id>/update', methods=('GET','POST'))
+@login_required
+def update(id):
+    """
+    Update a post if the current user is the author.
+    """
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if title is None:
+            error = 'Title is required.'
+        
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?', (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+    
+    return render_template('blog/update.html', post=post)
